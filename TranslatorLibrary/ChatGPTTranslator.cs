45f09441f778754726569f764ce5f8277576f65f
amos <124@mail.com>
2024-04-08 00:04:34 +0800

    修复

--- TranslatorLibrary/ChatGPTTranslator.cs
+++ TranslatorLibrary/ChatGPTTranslator.cs
@@ -40,7 +40,7 @@ namespace TranslatorLibrary
                 return null;
             }
             string retString;
-            string jsonParam = $"{{\"model\": \"{openai_model}\",\"messages\": [{{\"role\": \"system\", \"content\": \"你是一个翻译引擎。根据原文逐行翻译，将每行原文翻译为简体中文。保留每行文本的原始格式，并根据所需格式输出翻译后的文本。在翻译文本时，请严格注意以下几个方面：首先，一些完整的文本可能会被分成不同的行。请严格按照每行的原始文本进行翻译，不要偏离原文。其次，无论句子的长度如何，每行都是一个独立的句子，确保不要将多行合并成一个翻译。第三，在每行文本中，转义字符（例如\, \r, 和\n）或非日语内容（例如数字、英文字母、特殊符号等）不需要翻译或更改，应保持原样。\"}},{{\"role\": \"user\", \"content\": \"{q}\"}}]}}";
+            string jsonParam = $"{{\"model\": \"{openai_model}\",\"messages\": [{{\"role\": \"system\", \"content\": \"你是一个翻译引擎。根据原文逐行翻译，将每行原文翻译为简体中文。保留每行文本的原始格式，并根据所需格式输出翻译后的文本。在翻译文本时，请严格注意以下几个方面：首先，一些完整的文本可能会被分成不同的行。请严格按照每行的原始文本进行翻译，不要偏离原文。其次，无论句子的长度如何，每行都是一个独立的句子，确保不要将多行合并成一个翻译。第三，在每行文本中，转义字符（例如\, \\r, 和\\n）或非日语内容（例如数字、英文字母、特殊符号等）不需要翻译或更改，应保持原样。\"}},{{\"role\": \"user\", \"content\": \"{q}\"}}]}}";
             var hc = CommonFunction.GetHttpClient();
             var req = new StringContent(jsonParam, null, "application/json");
             hc.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", apiKey);
